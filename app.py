30bf67dd934c170b1a5417165dd46f0ad630c3e9
Miko1012 <mczarkowski7@outlook.com>
2020-10-28 06:10:14 +0100

    added file upload

--- app.py
+++ app.py
@@ -1,9 +1,14 @@
-import re
+import os
 import requests
 from flask import Flask, render_template, request
+from werkzeug.utils import secure_filename
+
+
+UPLOAD_FOLDER = './static/uploads'
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
 
 app = Flask(__name__)
-
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 @app.route('/')
 def hello_world():
@@ -20,9 +25,14 @@ def sender_register():
     print(request.values)
     print('~~~~')
     print(request.form)
+    file = request.files['photo']
+    filename = secure_filename(file.filename)
+    file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
     url = 'https://infinite-hamlet-29399.herokuapp.com/sender/register'
-    headers = {'Content-type': 'multipart/form-data; charset=UTF-8'}
-    response = requests.post(url, data=request.form, headers=headers)
+    response = requests.post(url, data=request.form)
+    print(request.form)
+    print("status-code")
+    print(response.status_code)
     if response.status_code != 200:
         return render_template('senderSignUp.html', message=response.text)
     return render_template('main.html')
