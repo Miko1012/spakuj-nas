4954b9e183aabd3a56c85feab42c6d006691f68b
Czarek` <mczarkowski7@outlook.com>
2020-10-29 03:20:19 +0100

    validated color-coded fields - not all yet

--- app.py
+++ app.py
@@ -3,12 +3,12 @@ import requests
 from flask import Flask, render_template, request
 from werkzeug.utils import secure_filename
 
-
-UPLOAD_FOLDER = './static/uploads'
+UPLOAD_FOLDER = './static/uploads/'
 ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
 
 @app.route('/')
 def hello_world():
@@ -22,17 +22,19 @@ def sender_sign_up():
 
 @app.route('/sender_register', methods=['POST'])
 def sender_register():
-    print(request.values)
-    print('~~~~')
-    print(request.form)
+    print('=== sending request to Chaberek ===')
     file = request.files['photo']
-    filename = secure_filename(file.filename)
-    file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+    if file:
+        filename = secure_filename(file.filename)
+        filepath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+        file.save(filepath)
+        print('found photo - name: ' + filename + ', filepath: ' + filepath)
+    print('request: ')
+    print(request)
+    print('request form: ')
+    print(request.form)
     url = 'https://infinite-hamlet-29399.herokuapp.com/sender/register'
     response = requests.post(url, data=request.form)
-    print(request.form)
-    print("status-code")
-    print(response.status_code)
     if response.status_code != 200:
         return render_template('senderSignUp.html', message=response.text)
     return render_template('main.html')
--- static/js/senderSignUp.js
+++ static/js/senderSignUp.js
@@ -1,4 +1,98 @@
-function registerSender(e) {
-    e.preventDefault();
-    console.log("aaa");
+validateForm = () => {
+
+}
+
+isValid = (field, value) => {
+    let LETTERS = 'AĄBCĆDEĘFGHIJKLŁMNŃOÓPQRSŚTUVWXYZŹŻ';
+    let letters = 'aąbcćdeęfghijklłmnńoópqrsśtuvwxyzźż';
+
+    switch (field) {
+        case 'firstname':
+        case 'lastname':
+            const nameRegexp = new RegExp('^[' + LETTERS + '][' + letters + ']+');
+            return nameRegexp.exec(value) !== null;
+        case 'sex':
+            console.log(value === 'M' || value === 'F');
+            return value === 'M' || value === 'F';
+        case 'login':
+        case 'password':
+            const passwordRegexp = new RegExp('.{8,}');
+            console.log(passwordRegexp.exec(value) !== null);
+            return passwordRegexp.exec(value) !== null;
+        case 'passwordRepeated':
+
+        case 'photo':
+
+    }
+}
+
+window.onload = () => {
+
+    let firstname = document.getElementById('firstname');
+    let lastname = document.getElementById('lastname');
+    let sex = document.getElementById('sex')
+    let login = document.getElementById('login');
+    let password = document.getElementById('password');
+    let passwordRepeated = document.getElementById('passwordRepeated');
+    let photo = document.getElementById('photo');
+
+
+    firstname.addEventListener('input', (e) => {
+        if(isValid('firstname', e.target.value)) {
+            firstname.classList.remove("invalid-field");
+            firstname.classList.add("valid-field");
+        } else {
+            firstname.classList.remove("valid-field");
+            firstname.classList.add("invalid-field");
+        }
+    });
+
+    lastname.addEventListener('input', (e) => {
+        if(isValid('lastname', e.target.value)) {
+            lastname.classList.remove("invalid-field");
+            lastname.classList.add("valid-field");
+        } else {
+            lastname.classList.remove("valid-field");
+            lastname.classList.add("invalid-field");
+        }
+    });
+
+    sex.addEventListener('input', (e) => {
+        if(isValid('sex', e.target.value)) {
+            sex.classList.remove("invalid-field");
+            sex.classList.add("valid-field");
+        } else {
+            sex.classList.remove("valid-field");
+            sex.classList.add("invalid-field");
+        }
+    });
+
+    login.addEventListener('input', (e) => {
+        if(isValid('login', e.target.value)) {
+            login.classList.remove("invalid-field");
+            login.classList.add("valid-field");
+        } else {
+            login.classList.remove("valid-field");
+            login.classList.add("invalid-field");
+        }
+    });
+
+    password.addEventListener('input', (e) => {
+        if(isValid('password', e.target.value)) {
+            password.classList.remove("invalid-field");
+            password.classList.add("valid-field");
+        } else {
+            password.classList.remove("valid-field");
+            password.classList.add("invalid-field");
+        }
+    });
+
+    passwordRepeated.addEventListener('input', (e) => {
+        isValid('passwordRepeated', e.target.value);
+    });
+
+    photo.addEventListener('change', (e) => {
+        isValid('firstname', e.target.value);
+    });
+
 }
